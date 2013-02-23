a154d742d7c5b4eaa739b14ca6be5c91afb04830
igio90 <giovanni.rocca.90@gmail.com>
2013-02-23 16:34:02 +0100

    update buildall
    Change-Id: If2309b1e9aef3cbbd655656c189840f22804f6f0

--- build-all.sh
+++ build-all.sh
@@ -2,9 +2,13 @@ sh bam-build.sh d2att;
 sh bam-build.sh d2spr;
 sh bam-build.sh d2tmo;
 sh bam-build.sh d2vzw;
+sh bam-build.sh endeavoru;
+sh bam-build.sh evita;
 sh bam-build.sh i9100;
 sh bam-build.sh i9100g;
 sh bam-build.sh i9300;
 sh bam-build.sh n7000;
 sh bam-build.sh n7100;
 sh bam-build.sh quincyatt;
+sh bam-build.sh ville;
+
