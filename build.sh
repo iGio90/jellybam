ab05629d54368d1399c389fff4f4fc45045208b4
igio90 <giovanni.rocca.90@gmail.com>
2013-02-23 13:06:30 +0100

    Colors
    Change-Id: I2e1bd118f9444ed8d70718db28c5147ecc27c524

--- build.sh
+++ build.sh
@@ -45,7 +45,7 @@ res1=$(date +%s.%N)
 echo -e '\0033\0143'
 clear
 
-echo -e "${cya}Building ${bldcya}B ${bldcya}A ${bldcya}M ${cya}v$VERSION ${txtrst}";
+echo -e "${cya}Building ${bldcya}B ${cya}A ${bldcya}M ${cya}v$VERSION ${txtrst}";
 
 # JellyBam device dependencies
 echo -e ""
