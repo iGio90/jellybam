da8414c7870798985148432b25e07c1eb1d521c1
igio90 <giovanni.rocca.90@gmail.com>
2013-02-23 13:05:05 +0100

    Added build-all script
    Change-Id: Ib4d9d8505e9d21a7d100f07c10be520db8dab11f

---
+++ build-all.sh
@@ -0,0 +1,10 @@
+sh bam-build.sh d2att;
+sh bam-build.sh d2spr;
+sh bam-build.sh d2tmo;
+sh bam-build.sh d2vzw;
+sh bam-build.sh i9100;
+sh bam-build.sh i9100g;
+sh bam-build.sh i9300;
+sh bam-build.sh n7000;
+sh bam-build.sh n7100;
+sh bam-build.sh quincyatt;
