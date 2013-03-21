207e8d8e8defc5b6e44507792654bed9e441d1c7
igio90 <giovanni.rocca.90@gmail.com>
2013-03-21 19:04:16 +0100

    Added lge_release script

---
+++ lge_release.sh
@@ -0,0 +1,70 @@
+echo "first of all, let's sync the repoes"
+sleep 5
+repo sync
+
+echo "great, now... to avoid device tree conflict it's better if we remove other manufacturer device tree"
+sleep 5
+
+if [ -d device/htc/ ]; then
+	echo "founded htc device tree... cleaning"
+	rm -r device/htc/
+else
+	echo "htc device tree not founded"
+fi
+if [ -d device/asus/ ]; then
+	echo "founded asus device tree... cleaning"
+	rm -r device/asus/
+else
+	echo "asus device tree not founded"
+fi
+if [ -d device/samsung/ ]; then
+	echo "founded samsung device tree... cleaning"
+	rm -r device/samsung/
+else
+	echo "lge device tree not founded"
+fi
+if [ -d device/sony/ ]; then
+	echo "founded sony device tree... cleaning"
+	rm -r device/sony"
+else
+	echo "sony device tree not founded"
+fi
+sleep 5
+
+echo "let's begin!"
+sleep5
+
+echo "Woaaaaah it's mako time!"
+sleep 5
+	sh bam-build.sh mako
+		scp out/target/product/mako/jellybam*.zip JellyBam@upload.goo.im:/home/JellyBam/mako/
+		scp out/target/product/mako/jellybam*.zip root@firecontrol.in:/home/Jellybam/upload/
+		ssh firecontrol.in touch /home/Jellybam/upload/start.txt
+
+echo "Woaaaaah it's p920 time!"
+sleep 5
+	sh bam-build.sh p920
+		scp out/target/product/p920/jellybam*.zip JellyBam@upload.goo.im:/home/JellyBam/p920/
+		scp out/target/product/o920/jellybam*.zip root@firecontrol.in:/home/Jellybam/upload/
+		ssh firecontrol.in touch /home/Jellybam/upload/start.txt
+
+echo "Woaaaaah it's p930 time!"
+sleep 5
+	sh bam-build.sh p930
+		scp out/target/product/p930/jellybam*.zip JellyBam@upload.goo.im:/home/JellyBam/p930/
+		scp out/target/product/p930/jellybam*.zip root@firecontrol.in:/home/Jellybam/upload/
+		ssh firecontrol.in touch /home/Jellybam/upload/start.txt
+
+echo "Woaaaaah it's p970 time!"
+sleep 5
+	sh bam-build.sh p970
+		scp out/target/product/p970/jellybam*.zip JellyBam@upload.goo.im:/home/JellyBam/p970/
+		scp out/target/product/p970/jellybam*.zip root@firecontrol.in:/home/Jellybam/upload/
+		ssh firecontrol.in touch /home/Jellybam/upload/start.txt
+
+echo "Woaaaaah it's p990 time!"
+sleep 5
+	sh bam-build.sh p990
+		scp out/target/product/p990/jellybam*.zip JellyBam@upload.goo.im:/home/JellyBam/p990/
+		scp out/target/product/p990/jellybam*.zip root@firecontrol.in:/home/Jellybam/upload/
+		ssh firecontrol.in touch /home/Jellybam/upload/start.txt
